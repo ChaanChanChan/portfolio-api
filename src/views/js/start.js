58797f13e34c4daa6e226582117fea079c1946a0
Chander <alexandertakepics@gmail.com>
2023-01-02 18:21:14 +0100

    header animation and queries done

--- src/views/js/start.js
+++ src/views/js/start.js
@@ -1,134 +1,223 @@
 //SPLIT WELCOME MESSAGE
-const welcome = new SplitType('#welcome')
+// const welcome = new SplitType('#welcome')
 
 //SPLIT TITLE
-const name = new SplitType('#alexander')
-const surname = new SplitType('#romero')
-const full = new SplitType('#full')
-const dev = new SplitType('#dev')
+const name = new SplitType('#name', { types: 'words' })
+const full = new SplitType('#full', { types: 'words' })
 
 //TIMELINE INTRO
-gsap.timeline()
-    .to('.header-content', {
-        display: "none"
-    })
-    //TRIANGLES
-    .from('.triangle-left', {
-        y: -400,
-        duration: 2,
-        opacity: 0,
-        ease: "circ.out"
-    }, 0)
-    .from('.triangle-right', {
-        y: 400,
-        duration: 2,
-        opacity: 0,
-        ease: "circ.out"
-    }, 0)
-    .to('.triangle-left', {
-        x: -300,
-        rotate: 360,
-        duration: 1,
-        ease: "circ.out"
-    }, 3)
-    .to('.triangle-right', {
-        x: 300,
-        rotate: 360,
-        duration: 1,
-        ease: "circ.out"
-    }, 3)
+// gsap.timeline()
+//     //TRIANGLES
+//     .from('.triangle-left', {
+//         y: -400,
+//         duration: 2,
+//         opacity: 0,
+//         ease: "circ.out"
+//     }, 0.5)
+//     .from('.triangle-right', {
+//         y: 400,
+//         duration: 2,
+//         opacity: 0,
+//         ease: "circ.out"
+//     }, 0.5)
+//     .to('.triangle-left', {
+//         x: -300,
+//         rotate: 360,
+//         duration: 1,
+//         ease: "circ.out"
+//     }, 3)
+//     .to('.triangle-right', {
+//         x: 300,
+//         rotate: 360,
+//         duration: 1,
+//         ease: "circ.out"
+//     }, 3)
 
-    //CHARACTERS
-    .to('.char', {
-        y: 0,
-        stagger: 0.05,
-        delay: 3.5,
-        duration: 0.05,
-        ease: "back.out(1.7)"
-    }, .5)
+//     //CHARACTERS
+//     .to('.char', {
+//         y: 0,
+//         stagger: 0.05,
+//         delay: 3.5,
+//         duration: 0.05,
+//         ease: "back.out(1.7)"
+//     }, .5)
 
 
 //ON CLICK PASS TO HEADER CONTENT FROM INTRO
-const welcomeBtn = document.querySelector('#welcome')
+// const welcomeBtn = document.querySelector('#welcome')
 
-welcomeBtn.addEventListener('click', () => {
-    gsap.timeline()
-        //CHARACTERS OUT
-        .to('.char', {
-            y: 120,
-            stagger: 0.05,
-            duration: 0.05,
-            ease: "back.out(1.7)"
-        })
+// welcomeBtn.addEventListener('click', () => {
+//     gsap.timeline()
+//         //CHARACTERS OUT
+//         .to('.char', {
+//             y: -120,
+//             stagger: 0.05,
+//             duration: 0.05,
+//             ease: "back.out(1.7)"
+//         })
 
-        //TRIANGLES OUT
-        .to('.triangle-left', {
-            y: -400,
-            duration: 2,
-            opacity: 0,
-            rotate: 180,
-            ease: "back.out(1.7)"
-        }, .5)
-        .to('.triangle-right', {
-            y: 400,
-            duration: 2,
-            opacity: 0,
-            rotate: 180,
-            ease: "back.out(1.7)"
-        }, .5)
+//         //TRIANGLES OUT
+//         .to('.triangle-left', {
+//             y: -400,
+//             duration: 2,
+//             opacity: 0,
+//             rotate: 180,
+//             ease: "back.out(1.7)"
+//         }, .5)
+//         .to('.triangle-right', {
+//             y: 400,
+//             duration: 2,
+//             opacity: 0,
+//             rotate: 180,
+//             ease: "back.out(1.7)"
+//         }, .5)
 
-        //INTRO OUT
-        .to('.intro', {
-            display: "none"
-        })
 
-        //HEADER CONTAINER IN
-        .to('.header-container', {
-            height: "100vh"
-        })
+//         //COVER IN
+//         .to('.cover', {
+//             height: "100vh",
+//             ease: "circ.out",
+//             duration: 1.5,
+//             delay: .3
+//         })
 
-        //HEADER CONTENT IN
-        .to('.header-content', {
-            display: "flex",
-            delay: 2
-        })
+//         //HEADER CONTAINER IN
+//         .to('.header-container', {
+//             display: "flex"
+//         })
 
-        //CONTENT ANIMATION
-        //TRIANGLE
-        .from('.triangle', {
-            ease: 'power1',
-            y: '250%',
-            opacity: 0,
-            duration: 1,
-            rotation: 90
-        })
-        //CHARACTERS
-        .from('#alexander .char', {
-            ease: "power1",
-            y: '20%',
-            opacity: 0,
-            stagger: .05,
-            duration: .2
-        })
-        .from('#romero .char', {
-            ease: "power1",
-            y: '20%',
-            opacity: 0,
-            stagger: .05,
-            duration: .2
-        })
-        .from('#full .char', {
-            ease: "power1",
-            y: '20%',
-            opacity: 0,
-            stagger: .05,
-            duration: .2
-        })
-        .from('#dev .char', {
-            ease: "power1",
-            y: '20%',
-            opacity: 0,
-            stagger: .05,
-            duration: .2
-        })
-})
+//         //INTRO OUT
+//         .to('.intro', {
+//             display: "none"
+//         })
+
+//         //HEADER CONTENT IN
+//         .to('.header-content', {
+//             display: "flex"
+//         })
+
+//         //CONTENT ANIMATION
+//         //TRIANGLE
+//         .from('.triangle', {
+//             ease: 'power1',
+//             y: '250%',
+//             opacity: 0,
+//             duration: 1,
+//             rotation: 90
+//         })
+
+//         //PORTRAIT IN
+//         .from('.portrait', {
+//             ease: "power1",
+//             x: -30,
+//             opacity: 0,
+//             delay: .1
+//         })
+//         // NAME IN
+//         .from(name.words, {
+//             ease: "power1",
+//             y: '20%',
+//             opacity: 0,
+//             stagger: .2,
+//             duration: 1
+//         })
+//         //FULL IN
+//         .from(full.words, {
+//             ease: "power1",
+//             y: '20%',
+//             opacity: 0,
+//             stagger: .2,
+//             duration: 1,
+//             delay: .1
+//         })
+// })
+
+gsap.timeline()
+    // //CHARACTERS OUT
+    // .to('.char', {
+    //     y: -120,
+    //     stagger: 0.05,
+    //     duration: 0.05,
+    //     ease: "back.out(1.7)"
+    // })
+
+    // //TRIANGLES OUT
+    // .to('.triangle-left', {
+    //     y: -400,
+    //     duration: 2,
+    //     opacity: 0,
+    //     rotate: 180,
+    //     ease: "back.out(1.7)"
+    // }, .5)
+    // .to('.triangle-right', {
+    //     y: 400,
+    //     duration: 2,
+    //     opacity: 0,
+    //     rotate: 180,
+    //     ease: "back.out(1.7)"
+    // }, .5)
+
+
+    //COVER IN
+    // .to('.cover', {
+    //     height: "100vh",
+    //     ease: "circ.out",
+    //     duration: 1.5,
+    //     delay: .3
+    // })
+
+    //HEADER CONTAINER IN
+    .to('.header-container', {
+        display: "flex"
+    })
+
+    //INTRO OUT
+    // .to('.intro', {
+    //     display: "none"
+    // })
+
+    //HEADER CONTENT IN
+    .to('.header-content', {
+        display: "flex"
+    })
+
+    //CONTENT ANIMATION
+    //TRIANGLE
+    .from('.triangle', {
+        ease: 'power1',
+        y: '250%',
+        opacity: 0,
+        duration: 1,
+        rotation: 90
+    })
+
+    //PORTRAIT IN
+    .from('.portrait', {
+        ease: "power1",
+        x: -30,
+        opacity: 0,
+        delay: .1
+    })
+    // NAME IN
+    .from(name.words, {
+        ease: "power1",
+        y: '20%',
+        opacity: 0,
+        stagger: .2,
+        duration: 1
+    })
+    //FULL IN
+    .from(full.words, {
+        ease: "power1",
+        y: '20%',
+        opacity: 0,
+        stagger: .2,
+        duration: 1,
+        delay: .1
+    })
+    .from('#nav ul li', {
+        ease: 'poser1',
+        x: '20%',
+        opacity: 0,
+        stagger: .3
+    })
